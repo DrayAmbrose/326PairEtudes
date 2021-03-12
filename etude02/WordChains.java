42c8f58d69a21a9607a125d93f87f7d2326dd529
jprakash <praja126@student.otago.ac.nz>
2021-03-12 13:36:18 +1300

    Added ability to input to two different arrays

--- etude02/WordChains.java
+++ etude02/WordChains.java
@@ -8,9 +8,11 @@
 */
 
 import java.util.*;
+import java.lang.StringBuilder;
 
 public class WordChains{
     private static String[] words;
+    private static String[] inputs;
 
     public static void main(String[] args){
         getInput();
@@ -18,18 +20,31 @@ public class WordChains{
 
      private static void getInput(){
         Scanner scan = new Scanner(System.in);
-	
+        /*Array lists to temporarily store the inputs and the list of words to use*/
         ArrayList<String> input = new ArrayList<String>();
-        int i = 0;
-	
+        ArrayList<String> wlist = new ArrayList<String>();
+        /*Iterate through file while it has lines and add the lines to respective array lists*/
         while(scan.hasNextLine()){
-            input.add(scan.nextLine().toLowerCase());
-            i++;
-        }
-        words = new String[input.size()];
-        words = input.toArray(words); 
-        for (String x:words){
-                System.out.println(x);
+            String winput =scan.nextLine();
+            int n =0;
+            /*Once the file reaches a blank line, it begins adding lines to the word list array*/
+            if(winput.equals("")){
+                n=1;
+                while(scan.hasNextLine()){
+                    winput = scan.nextLine();
+                    wlist.add(winput.toLowerCase());
+                }
+            }
+            if(n==0){ //As long as the blank line has not been encountered, keep adding words to the inputs array
+                input.add(winput.toLowerCase());
             }
         }
+        /* Convert array lists into string arrays */
+        words = new String[wlist.size()];
+        words = wlist.toArray(words);
+
+        inputs = new String[input.size()];
+        inputs= input.toArray(inputs);
+
+     }
 }
