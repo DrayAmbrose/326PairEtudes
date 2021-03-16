0fd7339f5bac4e63dc0129f062e4b9cbb1c4da58
Jayden <praja126@student.otago.ac.nz>
2021-03-16 16:27:20 +1300

    Put problems and dictionairy into two different arraylists

--- etude02/WordChains.java
+++ etude02/WordChains.java
@@ -11,40 +11,29 @@ import java.util.*;
 import java.lang.StringBuilder;
 
 public class WordChains{
-    private static String[] words;
-    private static String[] inputs;
 
     public static void main(String[] args){
         getInput();
+        
     }
 
      private static void getInput(){
-        Scanner scan = new Scanner(System.in);
-        /*Array lists to temporarily store the inputs and the list of words to use*/
-        ArrayList<String> input = new ArrayList<String>();
-        ArrayList<String> wlist = new ArrayList<String>();
-        /*Iterate through file while it has lines and add the lines to respective array lists*/
-        while(scan.hasNextLine()){
-            String winput =scan.nextLine();
-            int n =0;
-            /*Once the file reaches a blank line, it begins adding lines to the word list array*/
-            if(winput.equals("")){
-                n=1;
-                while(scan.hasNextLine()){
-                    winput = scan.nextLine();
-                    wlist.add(winput.toLowerCase());
-                }
-            }
-            if(n==0){ //As long as the blank line has not been encountered, keep adding words to the inputs array
-                input.add(winput.toLowerCase());
-            }
+        Scanner input = new Scanner(System.in);
+    ArrayList<String> problems = new ArrayList<>();
+    while (input.hasNextLine()) {
+      String prob = input.nextLine();
+      if (prob.trim().equals("")) {
+        break;
+      }
+      problems.add(prob);
+    }
+    ArrayList<String> words = new ArrayList<>();
+    while (input.hasNextLine()) {
+      words.add(input.nextLine());
+    }
+    for(String x: words){
+            System.out.println(x);
         }
-        /* Convert array lists into string arrays */
-        words = new String[wlist.size()];
-        words = wlist.toArray(words);
-
-        inputs = new String[input.size()];
-        inputs= input.toArray(inputs);
 
      }
 }
